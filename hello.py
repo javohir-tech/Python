c47c2cdc5d4981107858410326cd6e117053a18e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-18 20:46:19 +0500

    update

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("salom dunyo");
+hello = 'Salom Dunyo';
+
+print(hello);
+
+xabar = 'Salom';
+print(xabar);
+xabar = 'Dunyo';
+print(xabar);
+
+radius =  5;
+pi = 3.14159;
+aylana_yuzi = pi*radius**2;
+print(aylana_yuzi)
