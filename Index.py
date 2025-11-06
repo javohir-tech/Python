023b79539902a0a7dfe7c8c600257323a1d10e2a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-06 17:36:01 +0500

    if else

--- Index.py
+++ Index.py
@@ -1,41 +1,32 @@
-# Loops
-mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
-for mehmon in mehmonlar : 
-    print(f"hurmatli {mehmon} , sizni 20 dekaabr kuni nahorki oshga taaklif qilamiz")
-    print("Hurmat bilan Yusupovlar oilasi\n")
+avtolar = ['audi','bmw','volvo','kia','hyundai']
+
+for avto in avtolar : 
+    if avto == 'bmw': 
+        print(avto.upper())
+    else  :
+       print(avto.title())
+        
+print(avtolar)
+
+# ism = input('Ismingizni kiriting \n>>>')
+
+# if ism.lower()!='ali':
+#     print(f"uzr {ism} biz Alini kutayapmiz")
+# else: 
+#     print('salom Ali')
+
+javob = int(input('6*12 nechi boladi'))
+
+if javob != 72:
+    print('Xato Javob');
+else:
+    print('Togri');
     
-sonlar = list(range(1,11))
-sonlar_kvadrati  = []
-for son in sonlar :
-    sonlar_kvadrati.append(son**2);
-print(sonlar_kvadrati)
+yosh = int(input('Yoshingizni kiriting>>>'))
 
-# dostlar = [];
-# print("Dostlarni kiriting")
-# for n in range(5) : 
-#     dost = input(f"{n+1} dostni kiriting")
-#     dostlar.append(dost)
-# print(dostlar)
-
-names = ['javohir', 'bek', 'yahyo', 'jaahon', 'mansur']
-
-for name in names : 
-    print(f"salom {name}")
+if yosh >= 18 : 
+    print('hush kelipsiz')
+else:
+    print('yosh chagarasiga mos emas')
     
-newNums = list(range(10, 101));
-for num in newNums :
-    print(num**3)
     
-kinolar = []
-print('sevimli kinolar')
-for movie in range(5):
-     newMovie = input(f"{movie+1} kino nomini kiriting")
-     kinolar.append(newMovie.strip())
-print(kinolar)
-
-metting = []
-print('bugun kimlar bilan uchrashdiz')
-for peopl in range(5):
-    meet = input(f"{peopl+1}  odamni kiriting")
-    metting.append(meet)
-print(metting)
