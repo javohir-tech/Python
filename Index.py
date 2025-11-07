3ed7a98f258c7dc1a049f2bbe40235d81f13b62d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-07 23:03:10 +0500

    update

--- Index.py
+++ Index.py
@@ -1,35 +1,3 @@
-import math
-# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia'] 
-
-# for car in cars : 
-#     if car.lower() !=  'gm' : 
-#         # car.upper() 
-#         print(car.title())
-#     else : 
-#         # car.title()
-#         print(car.upper())
-        
-# login = input('Loginni kiriting ');
-# if login.lower() == 'admin' :
-#     print('Xush kelibsiz, Admin. Foydalanuvchilar ro\'yxatini ko\'rasizmi?')
-# else:
-#     print(f"hush kelipsiz {login}")
-    
-# a= int(input('a sonni kiriting '))
-# b= int(input('b sonni kiriting '))
-# if a==b : 
-#     print('sonlar teng');
-
-# javob = int(input('sonni kiriting'));
-# if javob <0:
-#     print('son manfiy');
-# else:
-#     print('son musbat')
-
-javob = int(input('Sonni kiritng '));
-
-if javob >=0 :
-    print(int(math.sqrt(javob)))
-else: 
-    print('Musbat son kiriting ')
-    
+for num in range(1, 101) :
+    if num % 2 == 0 :
+        print(num)
