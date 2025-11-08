035e3084153a4b0fc472eba57f58668880dbae49
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-08 19:34:47 +0500

    errore

--- javoblar-12-dars-04a.py
+++ javoblar-12-dars-04a.py
@@ -7,14 +7,16 @@ Web sahifa: https://python.sariq.dev
 """
 
 mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
-               'kartoshka', 'olma', 'banan', 'uzum', 'qovun'
+               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
 
+
+savat=[]
 
 for n in range(5):
     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
 
 if savat:
-    for mahsulot in savat
+    for mahsulot in savat :
         if mahsulot in mahsulotlar:
             print(f"Do'konimizda {mahsulot} bor")
         else:
