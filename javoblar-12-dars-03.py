dcd17a6017b07b25e0633bc0c68d69434f7fd17b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-08 19:31:48 +0500

    errore

--- javoblar-12-dars-03.py
+++ javoblar-12-dars-03.py
@@ -6,11 +6,11 @@ Muallif: Anvar Narzullaev
 Web sahifa: https://python.sariq.dev
 """
 
-x = float(input("Birinchi sonni kiriting: ")
-y = float(input("Ikkinchi sonni kiriting: ")
+x = float(input("Birinchi sonni kiriting: "))
+y = float(input("Ikkinchi sonni kiriting: "))
 if x==y:
     print(f"{x}={y}")
 elif x<y:
-    print(f'{x}<{y}")
-else
+    print(f"{x}<{y}")
+else:
     print(f"{x}>{y}")
