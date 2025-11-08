8b5c7509372b23fab99000a7d2a65f4daf8e9645
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-08 22:49:14 +0500

    dictionrys

--- Index.py
+++ Index.py
@@ -1,69 +1,102 @@
-# num = int(input("Sonni kiriting"));
+family = [
+    {
+        "who" : "Dadam", 
+        "name" : "ALimardon",
+        "year" : 1977
+    }, 
+    {
+        "who": "Onam ", 
+        "name" : "Xolida", 
+        "year" : 1982
+    }
+]
 
-# if num % 2== 0:
-#     print("raxmat")
-# else:
-#     print('bu juft son emas')
+for item in family :
+    print(f"{item["who"]} {item['year']} yilda tug'ilgan")
+    item["address"] = "Angren"
+    del item["address"]
+print(family)
 
-# age = float(input("Yoshingizni kiriting?"))
 
-# if age <=4 or age>=60 :
-#     price = 0
-# elif age<=18:
-#     price =10000;
-# elif age>18:
-#     price = 20000;
+talaba_0 = {
+    'ism':'alijon',
+    'familiya':'shamshiyev',
+    'yosh':22,
+    'fakultet':'matematika',
+    'kurs':4
+    }
+
+for kalit , qiymat in talaba_0.items() :
+    print(f"{kalit} : {qiymat}")
+
+mahsulotlar = { # Do'kondagi mahsulotlar
+    'olma':10000,
+    'anor':20000,
+    'uzum':40000,
+    'anjir':25000,
+    'shaftoli':30000
+    }
+
+print(sorted(mahsulotlar))
+for mahsulot in sorted(mahsulotlar):
+    print(mahsulot.title())
+
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s9',
+    'olim':'mi 10 pro',
+    'orif':'nokia 3310',
+    'hamida':'galaxy s9',
+    'maryam':'huawei p30',
+    'tohir':'iphone x',
+    'umar':'iphone x'    
+    }
+
+for tel in set(telefonlar.values()):
+    print(tel)
+
+davlatlar = {
+    "Aqsh" : "Washingtosh", 
+    "Russia" : "Maskva", 
+    "Uzbekistan" : "Toshkent", 
+    "Fransiya" : "Paris", 
+    "Italiya" : "Rim"
+}
+
+davlat = input("Davlatni kiriting >>> ").title()
+
+if davlat in davlatlar.keys() :
+    print(davlatlar[davlat])
+else: 
+    print('fuck is shet')
+print(davlatlar.get(davlat , 'fucck is shet'))
+for davlat in davlatlar.keys():
+    print(davlat)
     
-# print(f"Muzeyga kirish narxi {price}")
+for poytaxt in davlatlar.values():
+    print(poytaxt)
 
-# a = float(input("Birinchi son"))
-# b = float(input("Ikkinchi son"))
+taomlar  = {
+    "osh" : 20000, 
+    "palov": 150000,
+    "manti":30000, 
+    "somsa" : 10000, 
+    "lagmon" : 15000, 
+    "rolton" : 1000,
+    "chuchvra" : 30000  
+}
+buyurtmalar = []
+jami_summa =0 
+print("uchta ovqat buyurtma qiling !")
 
-# if a==b :
-#     print("Ikkala son teng")
-# else :
-#     katta = max(a, b)
-#     kichik = min(a, b)
-#     print(f"{kichik}<{katta}")
+for n in range(3):
+    buyurtma = input(f"{n+1} chi toam ni buyuring >>> ").lower()
+    buyurtmalar.append(buyurtma)
 
-# mahsulotlar = ['asal' , "kola" , "pepsi" , "snikers" , "gosht" , "pallmal", "caster" , "parlament"];
-
-# savat = []
-# bor_mahsulotlar= [];
-# mavjud_emas=[]
-
-# for n in range(5) : 
-#     item = input("Savatga qoshing ").lower();
-#     savat.append(item)
-
-# if savat :
-#     for item in savat :
-#         if item in mahsulotlar :
-#            bor_mahsulotlar.append(item)
-#         else: 
-#            mavjud_emas.append(item)
-#     if len(mavjud_emas)==0:
-#         print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
-#     else :
-#           print("quyidagi mahssulotlar dokonimizda yoq")
-#           for x in mavjud_emas :
-#            print(x)       
-# else:
-#     print("savat bosh") 
-
-# users= ['javohir' , 'jalol' , "bek" , 'ulugbek' , 'shaxi'];
-
-# login = input("Loginni kiriting     ").lower()
-
-# if login in users :
-#     print('login band')
-# else: 
-#     print('hush kelipsan ')     
-
-num = int(input('<<<Sonni kiriting>>>'));
-
-for n in range(2 , 11):
-    if num % n == 0:
-        print(f"{num} soni {n} ga qoldiqsiz bo'linadi")
-        
-    
+for buyurtma in buyurtmalar :
+    if buyurtma in taomlar.keys():
+        print(f"{buyurtma} narxi {taomlar[buyurtma]} so'm")
+        jami_summa+=taomlar[buyurtma]
+    else:
+        print(f"kechirasiz bizda {buyurtma} yo'q")
+print(f"Jammi sizdan {jami_summa} so'm boldi")
