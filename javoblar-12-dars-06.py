dbffad4942a4d24cf7557ef88a2428ed4eea504f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-08 19:39:28 +0500

    errore

--- javoblar-12-dars-06.py
+++ javoblar-12-dars-06.py
@@ -13,5 +13,5 @@ Web sahifa: https://python.sariq.dev
 son = int(input("Istalgan butun son kiriting: "))
 
 for n in range(2,11):
-    if not (son%n):
+    if son % n == 2 :
         print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
