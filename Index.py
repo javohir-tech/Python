c6c1e7a867bcb3f757cfcc9f0803e8ae0406cb13
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-09 22:15:31 +0500

    update

--- Index.py
+++ Index.py
@@ -1,9 +1,23 @@
-friends = {
-    "ali" : ['Taxtlar oyini' , "nimadir"],
-    "javohir" : ["breking bad " , "baxt"]
-}
+davlatlar = {
+    "o'zbekiston":{'poytaxt':"toshkent",
+                   'maydon':448978,
+                   'aholi':33_000_000,
+                   'pul birligi':"so'm"
+                   },
+    "rossiya":{'poytaxt':"moskva",
+                   'maydon':17_098_246,
+                   'aholi':144_000_000,
+                   'pul birligi':"rubl"
+                   },
+    "aqsh":{'poytaxt':"vashington",
+                   'maydon':9_631_418,
+                   'aholi':327_000_000,
+                   'pul birligi':"dollar"},
+    "malayziya":{'poytaxt':"kuala-lumpur",
+                   'maydon':329750,
+                   'aholi':25_000_000,
+                   'pul birligi':"rinngit"}
+    }
 
-for key , qiymat in friends.items() : 
-    print(f"{key} ning sevimli filmi quyidagilar " , end="")
-    for kino in qiymat : 
-        print(kino, end="")
+for davlat , malumot in davlatlar.items() :
+    print(f"{davlat} poytaxti {malumot["poytaxt"]}\n Hududi : {malumot['maydon']} kv.km \n Aholisi : {malumot["aholi"]} \n Pul birligi : {malumot['pul birligi']}")
