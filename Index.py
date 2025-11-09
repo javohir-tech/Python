657b450db48e97586ffb3f7740dda5267e1a94b8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-09 23:47:03 +0500

    update

--- Index.py
+++ Index.py
@@ -1,11 +1,29 @@
-buyurtmalar = []
-flag = True
-while flag :
-    buyurtma  = input("buyutma qiling . Buyurtmani to'xtatish uchun 'exit' deb yozing>>>");
-    if buyurtma.lower() == 'exit' :
-        flag = False
-        print('Buyurtmalar')
-        for buyurtma in buyurtmalar :
-            print(buyurtma)
-    else: 
-        buyurtmalar.append(buyurtma)
+# buyurtmalar = []
+# flag = True
+# while flag :
+#     buyurtma  = input("buyutma qiling . Buyurtmani to'xtatish uchun 'exit' deb yozing>>>");
+#     if buyurtma.lower() == 'exit' :
+#         flag = False
+#         print('Buyurtmalar')
+#         for buyurtma in buyurtmalar :
+#             print(buyurtma)
+#     else: 
+#         buyurtmalar.append(buyurtma)
+
+mahsulotlar = {}
+flag = True;
+stop = "to'xtatish uchun 'stop' deb yozing"
+while flag : 
+    mahsulot = input(f"mahsulotni kiriting {stop}>>> ")
+    if mahsulot.lower() == 'stop' :
+        for item, narx in mahsulotlar.items():
+            print(f"{item} : {narx}")
+            break
+    price = input(f"mahsulot narxini kiriting {stop}>>> ")
+    if mahsulot.lower() == 'stop' :
+        flag = False;
+        for item, narx in mahsulotlar.items():
+            print(f"{item} : {narx}")
+    
+    mahsulotlar[mahsulot] = int(price);
+
