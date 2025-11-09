00831d9e6b23a4ab99656d05e06597db4b5efd31
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-09 22:50:54 +0500

    update

--- Index.py
+++ Index.py
@@ -1,25 +1,12 @@
-# print("Kiritilgan sonning kvadratini qaytaruvchi dastur");
+print("Sevimli kinoblarizni kiriting");
 
-# savol = "Istalgan son kiriting";
-# savol+= "(dasturni to'xtatish uchun 'exit' deb yozing) : ";
-# flag = True
-# while flag:
-#          qiymat = input(savol)
-#          if qiymat == 'exit':
-#              break
-#          else:
-#              print(float(qiymat)**2)
-
-# for n in range(1, 11) :
-#     if n == 5 :
-#         continue
-#     print(n**2)
-    
-son = 0;
-
-while son < 10 : 
-    son+=1
-    if son == 3 :
-        continue
+savol = "(Dasturni to'xtatish uchun 'stop' deb yozing) ";
+flag = True
+n=1
+while flag :
+    qiymat = input(f"{n} chi sevimli kitob . {savol}")
+    if qiymat == 'stop':
+        flag = False
+        print('dastur tugadi>>>')
     else: 
-        print(son**2)
+        print(qiymat)
