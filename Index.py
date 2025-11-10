71fca4926e45e4ea485f42761f21f8e34b976d8b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-10 15:09:31 +0500

    update

--- Index.py
+++ Index.py
@@ -1,23 +1,41 @@
-users = []
-def userAdd(name , surname , year , address , email=None , tel=None):
-    user = {
-        "name" : name,
-        "surname" : surname, 
-        "birthday" : year, 
-        "age" : 2025-year, 
-        "address" : address, 
-        "email address" : email, 
-        "telNumber" : tel
-    }
-    users.append(user)
-    return user
+# users = []
+# def userAdd(name , surname , year , address , email=None , tel=None):
+#     user = {
+#         "name" : name,
+#         "surname" : surname,
+#         "birthday" : year,
+#         "age" : 2025-year,
+#         "address" : address,
+#         "email address" : email,
+#         "telNumber" : tel
+#     }
+#     return user
 
-name = input("Ismingiz : ")
-surname = input("Familya : ")
-t_yil = int(input("Tigulgan yilindiz : "))
-mazil = input("tugulgan joyigiz : ")
-emailManzil =  input("email manzilingiz : ")
-telNumber = input("Telefon raqam : ")
+# while True :
+#     print("\nQuyidagi ma'lumotlarni kiriting",end='')
+#     name = input("Ismingiz : ")
+#     surname = input("Familya : ")
+#     t_yil = int(input("Tigulgan yilindiz : "))
+#     mazil = input("tugulgan joyigiz : ")
+#     emailManzil =  input("email manzilingiz : ")
+#     telNumber = input("Telefon raqam : ")
 
-res =userAdd(name , surname , t_yil, mazil , emailManzil , telNumber);
-print(res)
+#     user = userAdd(name , surname , t_yil, mazil , emailManzil , telNumber);
+
+#     users.append(user)
+#     javob = input('davom ettiramizmi (yes/no)');
+
+#     if javob == 'no' :
+#         print(users)
+#         break
+
+
+# def maximum(param1, param2, param3):
+#     return max(param1, param2, param3)
+
+
+# res = maximum(1, 2, 3);
+# print(res)
+
+
+
