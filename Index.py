95c1f93495f32f568de085d72034f5dd340c9231
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-10 18:35:35 +0500

    update

--- Index.py
+++ Index.py
@@ -1,4 +1,24 @@
-from avto_info_mod import *
+# import math
+# from math import pi
+# print(math.sqrt(625))
+# print(math.pow(23, 2))
+# print(pi)
+# print(math.log2(8))
+# print(math.log10(100))
+# print(math.ceil(2.1))
+# print(math.exp(2))
 
-info1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000);
-info_print(info1)
+import random as r
+
+# son = r.randint(1, 100)
+# print(son)
+
+# ismlar = ['olim','anvar','hasan','husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choice(ism))
+
+x = list(range(0, 51, 5))
+print(x)
+r.shuffle(x)
+print(x)
