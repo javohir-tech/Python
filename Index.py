f33b3c89cb313a727767c812d6c41e682177a918
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-10 16:20:41 +0500

    update

--- Index.py
+++ Index.py
@@ -1,83 +1,25 @@
-# users = []
-# def userAdd(name , surname , year , address , email=None , tel=None):
-#     user = {
-#         "name" : name,
-#         "surname" : surname,
-#         "birthday" : year,
-#         "age" : 2025-year,
-#         "address" : address,
-#         "email address" : email,
-#         "telNumber" : tel
-#     }
-#     return user
-
-# while True :
-#     print("\nQuyidagi ma'lumotlarni kiriting",end='')
-#     name = input("Ismingiz : ")
-#     surname = input("Familya : ")
-#     t_yil = int(input("Tigulgan yilindiz : "))
-#     mazil = input("tugulgan joyigiz : ")
-#     emailManzil =  input("email manzilingiz : ")
-#     telNumber = input("Telefon raqam : ")
-
-#     user = userAdd(name , surname , t_yil, mazil , emailManzil , telNumber);
-
-#     users.append(user)
-#     javob = input('davom ettiramizmi (yes/no)');
-
-#     if javob == 'no' :
-#         print(users)
-#         break
+# def kattaHarf(ismlar) :
+#     qaytaradi = [];
+#     while ismlar : 
+#         katta = ismlar.pop().title();
+#         qaytaradi.append(katta)
+#     return qaytaradi;
 
 
-# def maximum(param1, param2, param3):
-#     return max(param1, param2, param3)
+# print(kattaHarf(ismlar[:]))
+# print(ismlar)
 
-
-# res = maximum(1, 2, 3);
-# print(res)
-
-# def cercle(radius) :
-#     PI = 3.14;
-#     return {
-#         "radiusi" : radius, 
-#         "diametr" : 2*radius, 
-#         "perimetr": 2*PI*radius, 
-#         "yuzi" : PI*radius**2 
-#     }
-
-# javob = int(input("aylana radiusini kiriting : >>>"))
-# res = cercle(javob)
-# print(res)
-
-# def tubSonlarTopish(chegara) : 
-#     tubSonlar = [];
-#     i=2;
-#     while True:
-#         neto = 0;
-#         qoshiladigan = i
-#         for n in range(1 , i) :
-#             if i % n == 0 :
-#                 neto+=1
-#             if neto >=2:
-#                 qoshiladigan = 0
-#                 break 
-#         if qoshiladigan != 0:
-#             tubSonlar.append(qoshiladigan)
-#         if len(tubSonlar) == chegara+1 :
-#             break
-#         i+=1                
-#     return tubSonlar 
-        
-# print(tubSonlarTopish(500))
-
-def fibonachi(n):
-    fibonachiSonlari = [1 , 1];
-    i = 2
-    while len(fibonachiSonlari) < n : 
-        item = fibonachiSonlari[i-1]+fibonachiSonlari[i-2]
-        fibonachiSonlari.append(item)
+def bahola(ismlar) :
+    baholar = {}
+    i =0 ;
+    while i < len(ismlar) :
+        ism = ismlar[i];
+        baho = int(input(f"{ism.title()} ga baho qoying >>> "))
+        baholar[ism.title()] = baho
         i+=1
-    return fibonachiSonlari
+    return baholar  
+        
+ismlar = ['ali', 'vali', 'hasan', 'husan']
 
-print(fibonachi(10))
+print(bahola(ismlar[:]))
+print(ismlar)
