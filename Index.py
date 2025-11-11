bfb0529634950ddd417630610b9370e24432fa5c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-11 23:52:26 +0500

    update

--- Index.py
+++ Index.py
@@ -1,8 +1,24 @@
 from transliterate import to_cyrillic , to_latin
+from dotenv import load_dotenv
+import os
+import telebot
 
-matn = input("Matn Kiriting ...")
+load_dotenv()
 
-if matn.isascii() :
-    print(to_cyrillic(matn))
-else:
-    print(to_latin(matn))
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Assalom Aleykum")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    xabar = message.text;
+    javob = lambda x : to_cyrillic(x) if x.isascii() else to_latin(x)
+    bot.reply_to(message , javob(xabar))
+
+bot.polling()
