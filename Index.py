9baa30994fc59e3858ca570988370f224aa8220c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-11 19:00:00 +0500

    update

--- Index.py
+++ Index.py
@@ -1,37 +1,37 @@
 from random import choice
 from words import sozlar
-def get_word():
+
+def get_word() :
     return choice(sozlar)
 
 def display(user_letters , word):
-    display_letter = ''
+    display_letters = '';
     for letter in word :
-        if letter in user_letters:
-            display_letter += letter
-        else :
-            display_letter += "_"
-    return display_letter
-
+        if letter  in user_letters :
+            display_letters+=letter
+        else:
+            display_letters+="_"
+    return display_letters
+            
 def play():
     word = get_word()
-    word_letters = set(word);
-    
-    user_letters = ''
-    print(f"men {len(word)} xonali soz o'yladim >>> ")
-    while word_letters :
-        print(display(user_letters , word))
-        if user_letters :
-            print(f"shu vaqtgacha kiritgan harflaringiz {user_letters}")
-        letter = input("harf kiritng >>>")
-        if letter in user_letters :
-            print("bu harfni kiritgansiz boshqa harf kiriting")
+    word_letters = set(word)
+    users_letters = " "
+    print(f"men {len(word)} uzunlikdagi soz oyladim top nahuy ");
+    while word_letters:
+        print(display(users_letters , word))
+        if  users_letters :
+            print(f"shu vaqtgacha kiritngan harflar {users_letters}")
+        letter = input("Harf kiring >>> ")
+        if letter  in users_letters :
+            print('bu harfni kirintgansiz !!!')
             continue
-        elif letter in word :
+        elif letter in word_letters :
             word_letters.remove(letter)
-            print(f"{letter} togri")
-        else :
-            print("bunday harf yoq")
-        user_letters += letter
-    print(f"tabriklaymiz siz {word} sozini {len(user_letters)} ta uruinoshda topdingiz");
+            print("siz kiritgan harf togri")
+        else:
+            print('bu hato')
+        users_letters+=letter
+    print(f"tabriklaymiz {word} urunish : {len(users_letters)}")
     
 play()
