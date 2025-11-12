a18b46c7708d11fe2c1465e8e1877411bc1a5643
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-12 14:14:04 +0500

    update

--- Index.py
+++ Index.py
@@ -1,24 +1,12 @@
-from transliterate import to_cyrillic , to_latin;
-from dotenv import load_dotenv
-
-import telebot
-import os
-
-load_dotenv()
-
-TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN , parse_mode=None)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message , "Assalom Aleykum")
+class Talaba :
+    def __init__(self , ism , familya , tyil):
+        self.name = ism.title();
+        self.surname = familya.title();
+        self.byear= tyil
+        
+    def get_name(self) :
+        return self.name
     
-@bot.message_handler(func = lambda m : True )
-def echo_all(message):
-    msg = message.text
-    javob = lambda x : to_cyrillic(x) if msg.isascii() else to_latin(x)
-    bot.reply_to(message , javob(msg))
-    
-
-
-bot.polling()
+talaba1 = Talaba("Javohir", "Suvonov" , 2004)
+print(talaba1.name)
+print(talaba1.get_name())
