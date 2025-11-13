9fbaa8238e51a33a48771de29e31de30b21f068e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-13 15:30:01 +0500

    files

---
+++ file.py
@@ -0,0 +1,25 @@
+
+# file = open('pi.txt')
+# PI = file.read()
+# print(PI)
+# file.close()
+
+# with open('pi.txt') as file :
+#     pi = file.read()
+    
+# print(pi)
+# pi = pi.rstrip()
+# pi = pi.replace("\n" , "")
+# pi = float(pi)
+# print(type(pi))
+# print(pi)
+
+# filename = 'Data/talabalar.txt'
+# with open(filename) as file :
+#     for line in file:
+#         print(line)
+
+# with open(filename) as file :
+#     talabalar = [talaba.rstrip() for talaba in file.readlines()]
+    
+# print(talabalar)
