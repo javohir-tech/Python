5e46432a28dc6387c316ab0a6d6d1d1266eab15f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-15 18:29:59 +0500

    pip and externall modules

--- req.py
+++ req.py
@@ -4,3 +4,10 @@ import pprint
 manzil = "https://kun.uz/news/main"
 res = requests.get(manzil)
 pprint.pprint(res.text)
+
+# country = "Uzbekistan"
+# url = f"https://restcountries.eu/rest/v2/name/{country}"
+# res = requests.get(url)
+# print(res.status_code)
+# r_json = res.json()
+# print(r_json)
