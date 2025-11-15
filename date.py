fc5a5c6792a3d4a6a31b2559728a7c22bd69022a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-15 14:05:10 +0500

    datetime module

---
+++ date.py
@@ -0,0 +1,38 @@
+import datetime as dt
+
+# hozir = dt.datetime.now()
+# print(hozir.date())
+# print(hozir.time())
+# print(hozir.hour)
+# print(hozir.minute)
+# print(hozir.second)
+
+# bugun = dt.date.today()
+# print(bugun)
+# ertaga = dt.date(2025 , 11, 16)
+# print(ertaga)
+
+# hozir = dt.datetime.now()
+# hozirgiVaqt= hozir.time()
+# print(hozirgiVaqt)
+
+# bugun = dt.date.today()
+# ramazon = dt.date(2026, 2, 17)
+# farq = ramazon - bugun
+# print(farq.days)
+
+# hozir = dt.datetime.now()
+# futbol = dt.datetime(2025, 11, 15, 22, 00, 00);
+# farq = futbol -hozir
+# seconds = farq.seconds
+# minutlar= int(seconds/60)
+# soatlar = int(minutlar/60)
+# print(f"futbol boshlanishiga {soatlar} qoldi")
+
+hozir = dt.datetime.now()
+vaqt = hozir.time().strftime("%H:%M:%S")
+print(vaqt)
+kun = hozir.date().strftime("%d-%m-%Y")
+print(kun)
+sana_vaqt= hozir.strftime("%d %h %Y yil, %H soat %M minut")
+print(sana_vaqt)
